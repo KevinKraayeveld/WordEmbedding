8b7261a9c781dbe5ce1c8c84ff25b01ac2bf3963
KevinKraayeveld <k.kraayeveld@mozard.nl>
2024-05-15 12:00:46 +0200

    Implement summation vectors

---
+++ src/Review-Vectorization/add_vectorization.R
@@ -0,0 +1,39 @@
+# List of required packages
+packages <- c("data.table")
+
+# Check if each package is installed, if not, install it
+for (package in packages) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+}
+
+library(data.table)
+
+start_time <- Sys.time()
+
+add_vectors <- function(token_index){
+  embeddings <- model[unlist(token_index),]
+  if(is.null(dim(embeddings)[1])){
+    vector <- embeddings
+  } else{
+    vector <- colSums(embeddings)
+  }
+  return(vector)
+}
+
+df[, Review_Vector := lapply(df$Token_index, function(tokens){
+  add_vectors(tokens)
+})]
+
+# Total execution time
+total_execution_time <- as.numeric(difftime(end_time, start_time, units = "secs"))
+cat("Total execution time of review vectorization:", total_execution_time, "seconds \n")
+cat("Estimated execution time for full dataset is", total_execution_time*(4000000/nrow(df)), 
+    "seconds. Which is", total_execution_time*(4000000/nrow(df))/3600, "hours \n")
+
+print("remove model from working session")
+#rm(model)
+
+print("remove unnecessary columns")
+df[, Token_index := NULL]
