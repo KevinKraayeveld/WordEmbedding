3cbe0d966c6ce2f5d004da44c9500a568f0c304b
KevinKraayeveld <k.kraayeveld@mozard.nl>
2024-05-29 11:56:17 +0200

    Read df and test files in R after running ELMo.py

--- src/Embedding-Models/ELMo.R
+++ src/Embedding-Models/ELMo.R
@@ -15,3 +15,14 @@ use_python(python_path)
 py_run_string(paste0("small_data = '", small_data, "'"))
 py_run_string(paste0("preprocessing_method = '", preprocessing_method, "'"))
 source_python("ELMo.py")
+
+if(small_data){
+  df_path <- paste0("../data/", preprocessing_method, "_elmo_train_small.csv")
+  test_path  <- paste0("../data/", preprocessing_method, "_elmo_test_small.csv")
+}else {
+  df_path <- paste0("../data/", preprocessing_method, "_elmo_train.csv")
+  test_path  <- paste0("../data/", preprocessing_method, "_elmo_test.csv")
+}
+
+df <- fread(df_path)
+test <- fread(test_path)
