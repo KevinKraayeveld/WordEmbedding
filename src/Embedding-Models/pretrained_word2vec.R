868dee7bbaf27fc0ffb7da01cfc7896a496726ae
KevinKraayeveld <k.kraayeveld@mozard.nl>
2024-06-17 15:44:53 +0200

    Turn pretrained_word2vec model to matrix before vectorizing

--- src/Embedding-Models/pretrained_word2vec.R
+++ src/Embedding-Models/pretrained_word2vec.R
@@ -6,5 +6,42 @@ use_python(python_path)
 gensim <- import("gensim")
 
 model_path <- "../pretrained_models/GoogleNews-vectors-negative300.bin"
-model <- gensim$models$KeyedVectors$load_word2vec_format(model_path, binary = TRUE)
+gensim_model <- gensim$models$KeyedVectors$load_word2vec_format(model_path, binary = TRUE)
+
+if(small_data){
+  words_path <- paste0("../data/variables/", preprocessing_method, "_words_small.rds")
+  test_vocabulary_path <- paste0("../data/variables/", preprocessing_method, "_test_vocabulary_small.rds")
+}else{
+  words_path <- paste0("../data/variables/", preprocessing_method, "_words.rds")
+  test_vocabulary_path <- paste0("../data/variables/", preprocessing_method, "_test_vocabulary.rds")
+}
+train_vocabulary_terms <- readRDS(words_path)
+test_vocabulary <- readRDS(test_vocabulary_path)
+
+rm(test_vocabulary_path, words_path)
+
+words <- union(train_vocabulary_terms, test_vocabulary$term)
+
+rm(train_vocabulary_terms, test_vocabulary)
+
+embeddings_list <- list()
+
+for (word in words){
+  tryCatch({
+    embedding <- gensim_model$get_vector(word)
+    embeddings_list[[word]] <- embedding
+  }, error = function(e){
+    
+  })
+}
+
+model <- do.call(rbind, embeddings_list)
+
+rm(embeddings_list)
+
+if(small_data){
+  saveRDS(model, "../data/models/pretrained_word2vec_small.rds")
+} else{
+  saveRDS(model, "../data/models/pretrained_word2vec.rds")
+}
 
--- src/Review-Vectorization/get_embeddings.R
+++ src/Review-Vectorization/get_embeddings.R
@@ -11,29 +11,7 @@ for (package in packages) {
 library(data.table)
 library(fastTextR)
 
-if(embedding_model == "pretrained_word2vec"){
-  get_embeddings <- function(tokens) {
-    # Initialize an empty matrix to store embeddings
-    embedding_size <- model$vector_size
-    embeddings_matrix <- matrix(NA, nrow = length(tokens), ncol = embedding_size)
-    
-    # Fill the matrix with embeddings
-    for (i in seq_along(tokens)) {
-      token <- tokens[[i]]
-      embedding <- tryCatch({
-        model$get_vector(token)
-      }, error = function(e) {
-        rep(NA, embedding_size)
-      })
-      embeddings_matrix[i, ] <- embedding
-    }
-  
-    # Assign row names
-    rownames(embeddings_matrix) <- tokens
-    
-    return(embeddings_matrix)
-  }
-} else if(embedding_model == "pretrained_fastText"){
+if(embedding_model == "pretrained_fastText"){
   get_embeddings <- function(tokens){
     ft_word_vectors(model, tokens)
   }
